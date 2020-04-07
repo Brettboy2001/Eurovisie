dacb23d43725cf797f59eac144d36a3ad315911f
ColinvD <22950@ma-web.nl>
2020-04-07 16:58:46 +0200

    added that the AI can use multiple buttons and starts when song starts

--- Assets/Scripts/AI/RandomButtonPresser.cs
+++ Assets/Scripts/AI/RandomButtonPresser.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Eurovision.Gameplay;
 //using Eurovision.Input;
 using TMPro;
 using UnityEngine;
@@ -9,31 +10,67 @@ namespace Eurovision.Input
     public class RandomButtonPresser : InputReader
     {
         private InputHandler _handler;
-        private int data;
+        private TaskTracker _tracker;
+        [SerializeField] private List<int> datas;
+        [SerializeField] private int _minimumEffects = 1;
+        [SerializeField] private int _maximumEffects;
+        [SerializeField] private float _setupCooldown = 5f;
+        [SerializeField] private float _particleCooldown = 5f;
     
         // Start is called before the first frame update
         void Start()
         {
             _handler = FindObjectOfType<InputHandler>();
+            _tracker = FindObjectOfType<TaskTracker>();
+            _tracker.OnTaskComplete += StartAI;
+            //StartCoroutine(AI());
+        }
+
+        private void StartAI()
+        {
             StartCoroutine(AI());
+            _tracker.OnTaskComplete -= StartAI;
         }
 
         // Update is called once per frame
         void Update()
         {
-            Debug.Log("Data: " + data);
-            ParseInput(data);
+            //ParseInput(data);
+            for (int i = 0; i < datas.Count; i++)
+            {
+                ParseInput(datas[i]);
+            }
         }
         private IEnumerator AI()
         {
+            datas = new List<int>();
             //int data;
             while (true)
             {
-                yield return new WaitForSeconds(5f);
-                int temp = Random.Range(0, _handler.ButtonAmount);
-                data = (temp + 1) * 10 + 1;
-                yield return new WaitForSeconds(5f);
-                data = (temp+1) * 10;
+                int wantingEffects = Random.Range(_minimumEffects, _maximumEffects); // amount of effects i want to activate
+                List<int> availableNum = new List<int>(_handler.ButtonAmount); // what numbers can i choose from
+                List<int> effects = new List<int>(wantingEffects); // all effects i chose
+                for (int i = 0; i < availableNum.Capacity; i++)
+                {
+                    availableNum.Add(i);
+                }
+                for (int i = 0; i < effects.Capacity; i++)
+                {
+                    int randNum = Random.Range(0, availableNum.Count);
+                    effects.Add(availableNum[randNum]);
+                    availableNum.RemoveAt(randNum);
+                }
+                yield return new WaitForSeconds(_setupCooldown);
+                datas.Clear();
+                for (int i = 0; i < effects.Count; i++)
+                {
+                    datas.Add((effects[i] + 1) * 10 + 1);
+                }
+                yield return new WaitForSeconds(_particleCooldown);
+                for (int i = 0; i < effects.Count; i++)
+                {
+                    datas[i] = (effects[i] + 1) * 10;
+                }
             }
         }
     }
