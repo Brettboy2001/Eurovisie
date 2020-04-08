16f39ec895a59ec230ba0f1689ec360a419f7899
ColinvD <22950@ma-web.nl>
2020-04-08 12:01:13 +0200

    moved some of the code to new functions

--- Assets/Scripts/AI/RandomButtonPresser.cs
+++ Assets/Scripts/AI/RandomButtonPresser.cs
@@ -1,8 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using Eurovision.Gameplay;
-//using Eurovision.Input;
-using TMPro;
 using UnityEngine;
 
 namespace Eurovision.Input
@@ -12,9 +10,10 @@ namespace Eurovision.Input
         private InputHandler _handler;
         private TaskTracker _tracker;
         [SerializeField] private List<int> datas;
+        private List<int> effects;
         [SerializeField] private int _minimumEffects = 1;
-        [SerializeField] private int _maximumEffects;
-        [SerializeField] private float _setupCooldown = 5f;
+        [SerializeField] private int _maximumEffects = 4;
+        [SerializeField] private float _setupCooldown = 2f;
         [SerializeField] private float _particleCooldown = 5f;
     
         // Start is called before the first frame update
@@ -23,7 +22,6 @@ namespace Eurovision.Input
             _handler = FindObjectOfType<InputHandler>();
             _tracker = FindObjectOfType<TaskTracker>();
             _tracker.OnTaskComplete += StartAI;
-            //StartCoroutine(AI());
         }
 
         private void StartAI()
@@ -35,7 +33,6 @@ namespace Eurovision.Input
         // Update is called once per frame
         void Update()
         {
-            //ParseInput(data);
             for (int i = 0; i < datas.Count; i++)
             {
                 ParseInput(datas[i]);
@@ -44,33 +41,41 @@ namespace Eurovision.Input
         private IEnumerator AI()
         {
             datas = new List<int>();
-            //int data;
             while (true)
             {
-                int wantingEffects = Random.Range(_minimumEffects, _maximumEffects); // amount of effects i want to activate
-                List<int> availableNum = new List<int>(_handler.ButtonAmount); // what numbers can i choose from
-                List<int> effects = new List<int>(wantingEffects); // all effects i chose
-                for (int i = 0; i < availableNum.Capacity; i++)
-                {
-                    availableNum.Add(i);
-                }
-                for (int i = 0; i < effects.Capacity; i++)
-                {
-                    int randNum = Random.Range(0, availableNum.Count);
-                    effects.Add(availableNum[randNum]);
-                    availableNum.RemoveAt(randNum);
-                }
                 yield return new WaitForSeconds(_setupCooldown);
-                datas.Clear();
-                for (int i = 0; i < effects.Count; i++)
-                {
-                    datas.Add((effects[i] + 1) * 10 + 1);
-                }
+                StartRandomEffects();
                 yield return new WaitForSeconds(_particleCooldown);
-                for (int i = 0; i < effects.Count; i++)
-                {
-                    datas[i] = (effects[i] + 1) * 10;
-                }
+                StopEffects();
+            }
+        }
+
+        public void StartRandomEffects()
+        {
+            int wantingEffects = Random.Range(_minimumEffects, _maximumEffects);
+            List<int> availableNum = new List<int>(_handler.ButtonAmount);
+            effects = new List<int>(wantingEffects);
+            for (int i = 0; i < availableNum.Capacity; i++)
+            {
+                availableNum.Add(i);
+            }
+            for (int i = 0; i < effects.Capacity; i++)
+            {
+                int randNum = Random.Range(0, availableNum.Count);
+                effects.Add(availableNum[randNum]);
+                availableNum.RemoveAt(randNum);
+            }
+            datas.Clear();
+            for (int i = 0; i < effects.Count; i++)
+            {
+                datas.Add((effects[i] + 1) * 10 + 1);
+            }
+        }
+        public void StopEffects()
+        {
+            for (int i = 0; i < effects.Count; i++)
+            {
+                datas[i] = (effects[i] + 1) * 10;
             }
         }
     }
